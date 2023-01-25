2a429ff8497a489db66416a2582829a277e36568
Mario Karajallo <mariokarajallo92@gmail.com>
2023-01-24 23:46:23 -0300

    feat: spider function  start_resquests

--- Modulo 5 - Scrapy, Tesseract y Proxies/linkedin_jobs/linkedin_jobs/spiders/linkedin_jobs_spider.py
+++ Modulo 5 - Scrapy, Tesseract y Proxies/linkedin_jobs/linkedin_jobs/spiders/linkedin_jobs_spider.py
@@ -3,8 +3,11 @@ import scrapy
 
 class LinkedinJobsSpiderSpider(scrapy.Spider):
     name = 'linkedin_jobs_spider'
-    allowed_domains = ['linkedin.com']
-    start_urls = ['http://linkedin.com/']
+    api_url = 'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=python&location=United%2BStates&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&start=' 
+    # allowed_domains = ['linkedin.com']
+    # start_urls = ['http://linkedin.com/']
 
-    def parse(self, response):
-        pass
+    def start_requests(self):
+        first_job_on_page = 0
+        first_url = self.api_url + str(first_job_on_page)
+        yield scrapy.Request(url=first_url, callback=self.parse_job, meta={'first_job_on_page': first_job_on_page})
