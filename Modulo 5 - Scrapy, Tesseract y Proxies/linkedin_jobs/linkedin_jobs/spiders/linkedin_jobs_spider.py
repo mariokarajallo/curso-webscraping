76a071391ce4f4a92d7c2b33b0e907bd63a066df
Mario Karajallo <mariokarajallo92@gmail.com>
2023-01-25 23:10:46 -0300

    feat: parse job  add condition fro resquets next page of jobs

--- Modulo 5 - Scrapy, Tesseract y Proxies/linkedin_jobs/linkedin_jobs/spiders/linkedin_jobs_spider.py
+++ Modulo 5 - Scrapy, Tesseract y Proxies/linkedin_jobs/linkedin_jobs/spiders/linkedin_jobs_spider.py
@@ -12,23 +12,29 @@ class LinkedinJobsSpiderSpider(scrapy.Spider):
         yield scrapy.Request(url=first_url, callback=self.parse_job, meta={'first_job_on_page': first_job_on_page})
 
     def parse_job(self, response):
-            first_job_on_page = response.meta['first_job_on_page']
+        first_job_on_page = response.meta['first_job_on_page']
 
-            job_item = {}
-            jobs = response.css("li")
+        job_item = {}
+        jobs = response.css("li")
 
-            num_jobs_returned = len(jobs)
-            print("******* Num Jobs Returned *******")
-            print(num_jobs_returned)
-            print('*****')
+        num_jobs_returned = len(jobs)
+        print("******* Num Jobs Returned *******")
+        print(num_jobs_returned)
+        print('*****')
+        
+        for job in jobs:
             
-            for job in jobs:
-                
-                job_item['job_title'] = job.css("h3::text").get(default='not-found').strip()
-                job_item['job_detail_url'] = job.css(".base-card__full-link::attr(href)").get(default='not-found').strip()
-                job_item['job_listed'] = job.css('time::text').get(default='not-found').strip()
+            job_item['job_title'] = job.css("h3::text").get(default='not-found').strip()
+            job_item['job_detail_url'] = job.css(".base-card__full-link::attr(href)").get(default='not-found').strip()
+            job_item['job_listed'] = job.css('time::text').get(default='not-found').strip()
 
-                job_item['company_name'] = job.css('h4 a::text').get(default='not-found').strip()
-                job_item['company_link'] = job.css('h4 a::attr(href)').get(default='not-found')
-                job_item['company_location'] = job.css('.job-search-card__location::text').get(default='not-found').strip()
-                yield job_item
+            job_item['company_name'] = job.css('h4 a::text').get(default='not-found').strip()
+            job_item['company_link'] = job.css('h4 a::attr(href)').get(default='not-found')
+            job_item['company_location'] = job.css('.job-search-card__location::text').get(default='not-found').strip()
+            yield job_item
+        
+        #### REQUEST NEXT PAGE OF JOBS HERE ######
+        if num_jobs_returned > 0:
+            first_job_on_page = int(first_job_on_page) + 25
+            next_url = self.api_url + str(first_job_on_page)
+            yield scrapy.Request(url=next_url, callback=self.parse_job, meta={'first_job_on_page': first_job_on_page})
